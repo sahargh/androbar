505731dda8e3d77a8675d6477918749571b07a7c
rumyhuampas@gmail.com <rumyhuampas@gmail.com@597cec89-03b0-f37b-4e6b-a252dcebf3ac>
2012-09-09 01:19:19 +0000

    First Upload

--- AndroBar/src/andro/bar/Welcome.java
+++ AndroBar/src/andro/bar/Welcome.java
@@ -3,13 +3,13 @@ package andro.bar;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity
+public class Welcome extends Activity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.welcome);
     }
 }
